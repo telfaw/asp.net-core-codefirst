5949b04764b72e9fc6bbc635f170b580469c1781
fawaz M G Abdulghani <telfaw@gmail.com>
2023-12-15 13:32:59 +0300

    first asp.net
    تحميل الملفات الاساسية

---
+++ Items.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestCoreWebAppMVC.Models
+{
+    public class Items
+    {
+        
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get ; set; }
+        public string Description { get; set; } 
+        public string Title { get; set; }
+        [Required]
+        public decimal Price  { get; set; }
+        [Required]
+        public decimal Qty { get; set; }
+        public decimal Total { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.Now; 
+        
+
+    }
+}
